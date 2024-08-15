2e920f189940f338e8cec4be047e991706126349
Vinicius Evangelista <vini.evangelista@live.com>
2024-08-14 23:34:34 -0300

    feat: doing exercise one

--- sales.py
+++ sales.py
@@ -1,4 +1,27 @@
-print("Bem-vindo a loja do Vinicius")
+print("Bem-vindo a loja do Vinicius do Nascimento Evangelista")
 
-valorDoPedido = 0
-valorDaParcela = 0
+print("Entre com o valor do pedido: ")
+valorDoPedido = float(input())
+
+print("Entre com a quantidade de parcelas: ")
+quantidadeParcelas = int(input())
+
+
+juros = 0
+
+if (quantidadeParcelas < 4): 
+    juros = 0
+elif (quantidadeParcelas >= 6 & quantidadeParcelas < 9):
+    juros = 8/100
+elif (quantidadeParcelas >= 9 & quantidadeParcelas < 13):
+    juros = 16/100
+else:
+    juros = 32/100
+
+
+
+valorDaParcela = (valorDoPedido * (1 +  juros)) / quantidadeParcelas
+
+valorTotal = valorDaParcela * quantidadeParcelas
+
+print(valorTotal)
