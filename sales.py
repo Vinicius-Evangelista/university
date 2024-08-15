9e1a01146b633fab009a82f165129fb943c0439e
vinicius <vinicius.evangelista@sp.senai.br>
2024-08-15 10:35:16 -0300

    feat: complete first exercise

--- sales.py
+++ sales.py
@@ -1,16 +1,17 @@
 print("Bem-vindo a loja do Vinicius do Nascimento Evangelista")
 
-print("Entre com o valor do pedido: ")
+print("Entre com o valor do pedido: ", end="")
 valorDoPedido = float(input())
 
-print("Entre com a quantidade de parcelas: ")
+print("Entre com a quantidade de parcelas: ", end="")
 quantidadeParcelas = int(input())
-
 
 juros = 0
 
 if (quantidadeParcelas < 4): 
     juros = 0
+elif (quantidadeParcelas >= 4 & quantidadeParcelas < 6):
+    juros = 4/100
 elif (quantidadeParcelas >= 6 & quantidadeParcelas < 9):
     juros = 8/100
 elif (quantidadeParcelas >= 9 & quantidadeParcelas < 13):
@@ -18,10 +19,9 @@ elif (quantidadeParcelas >= 9 & quantidadeParcelas < 13):
 else:
     juros = 32/100
 
+valorDaParcela = round(float((valorDoPedido * (1 +  juros)) / quantidadeParcelas), 2)
 
+print(f"O valor das parcelas é: R$ {valorDaParcela}", end="")
 
-valorDaParcela = (valorDoPedido * (1 +  juros)) / quantidadeParcelas
-
-valorTotal = valorDaParcela * quantidadeParcelas
-
-print(valorTotal)
+valorTotal = round(valorDaParcela * quantidadeParcelas, 2)
+print(f"O valor Total Parcelado é: R$ {valorTotal}", end="")
