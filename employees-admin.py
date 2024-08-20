3ff5b00865575c4a936f77c0a2688eb608856d88
Vinicius Evangelista <vini.evangelista@live.com>
2024-08-19 23:24:45 -0300

    feat: add new emploeyee admin console app and shirt commerce app

--- employees-admin.py
+++ employees-admin.py
@@ -1,1 +1,100 @@
 print("Bem vindos a empresa do Vinicius do Nascimento Evangelista")
+
+lista_de_funcionarios = []
+
+id_global = 4676461
+
+def cadastrar_funcionario(id):
+    funcionario = {
+        "id" : id,
+        "nome" : "",
+        "setor" : "",
+        "salario" : 0
+    }
+
+    print("--------- CADASTRAR FUNCIONÁRIOS ----------")
+    funcionario["nome"] = input("Por favor entre com o nome do funcionário: ")
+    funcionario["setor"] = input("Por favor entre com o setor do funcionário: ")
+    funcionario["salario"] = input("Por favor entre com o salário do funcionário: ")
+    
+    lista_de_funcionarios.append(funcionario.copy())
+
+def consultar_funcionarios():
+    while True:
+        print("--------- CONSULTAR FUNCIONÁRIOS ----------")
+        print("1 - Consultar todos os funcionário")
+        print("2 - Consultar Funcionários por Id")
+        print("3 - Consultar Funcionários por setor")
+        print("4 - Retornar")
+        opcao = int(input())
+
+        if opcao == 1:
+            for funcionario in lista_de_funcionarios:
+                print("-----------------------------------")
+                print(f"id : {funcionario['id']}")
+                print(f"nome: {funcionario['nome']}")
+                print(f"setor: {funcionario['setor']}")
+                print(f"salário: {funcionario['salario']}")
+                print("-----------------------------------")
+        elif opcao == 2:
+            id = int(input("Digite o id do funcionário: "))
+            for funcionario in lista_de_funcionarios:
+                if funcionario["id"] == id: 
+                    print("-----------------------------------")
+                    print(f"id : {funcionario['id']}")
+                    print(f"nome: {funcionario['nome']}")
+                    print(f"setor: {funcionario['setor']}")
+                    print(f"salário: {funcionario['salario']}")
+                    print("-----------------------------------")
+        elif opcao == 3:
+            setor = input("Digite o setor do funcionário: ")
+            for funcionario in lista_de_funcionarios:
+                if funcionario["setor"] == setor: 
+                    print("-----------------------------------")
+                    print(f"id : {funcionario['id']}")
+                    print(f"nome: {funcionario['nome']}")
+                    print(f"setor: {funcionario['setor']}")
+                    print(f"salário: {funcionario['salario']}")
+                    print("-----------------------------------")
+        elif opcao == 4:
+            break
+        else:
+            print("Opção inválida")
+ 
+
+def remove_funcionario():
+    while True:
+        print("--------- CONSULTAR FUNCIONÁRIOS ----------")
+        id = int(input("Digite o id do funcionário a ser removido: "))
+
+        for funcionario in lista_de_funcionarios:
+            if funcionario["id"] == id:
+                lista_de_funcionarios.remove(funcionario)
+                print("Funcionário removido com sucesso!")
+                return 
+
+        print("id inválido!")
+         
+def main():
+    while True:
+        print("--------- MENU PRINCIPAL ----------")
+        print("1 - Cadastrar funcionário")
+        print("2 - Consultar funcionário(s)")
+        print("3 - Remover funcionários")
+        print("4 - Sair")
+        opcao = int(input())
+
+        if opcao == 1: 
+            global id_global 
+            id_global += 1
+            cadastrar_funcionario(id_global)
+        elif opcao == 2:
+            consultar_funcionarios()
+        elif opcao == 3:
+            remove_funcionario()
+        elif opcao == 4:
+            break
+        else:
+            print("Opção inválida!")
+
+main()
--- shirt-commerce.py
+++ shirt-commerce.py
@@ -15,26 +15,60 @@ modelosEvalores = {
 
 def escolha_modelo():
     while True:
-         modelo = input("Qual o modelo desejado?").upper()
+         modelo = input("Qual o modelo desejado? ").upper()
          if not (modelo in modelosEvalores.keys()):
              print("Escolha inválida, entre com outro modelo novamente")
          else: 
              break
     return modelosEvalores[modelo]
 
-def num_camiseta(modelo):
-    try: 
-         camisetas = int(input("Entre com o número de camisetas: "))
-         if camisetas < 20:
-             return 0
-         elif camisetas >= 20 & camisetas < 200:
-             return 5/100
-         elif camisetas >= 200 & camisetas < 2000:
-             return 7/100
-         elif camisetas >= 2000 & camisetas < 20000:
-             return 12/100
-         else:
-            print("Não aceitamos tantas camisetas de uma vez")
-    except:
-        print("Input inválido!")
+def num_camiseta():
+    while True: 
+         try: 
+              camisetas = int(input("Entre com o número de camisetas: "))
+              if camisetas < 20:
+                return 0
+              elif camisetas >= 20 and camisetas < 200:
+                return camisetas * 95/100 
+              elif camisetas >= 200 and camisetas < 2000:
+                return camisetas * 93/100
+              elif camisetas >= 2000 and camisetas < 20000:
+                return camisetas * 88/100
+              elif camisetas > 20000:
+                 print("Não aceitamos tantas camisetas de uma vez")
+         except:
+             print("Input inválido!")
+         print("Entre com o valor da camiseta novamente")
 
+
+def frete():
+    while True:
+        try:
+             print("1 - Frete por transportadora - R$100.00")
+             print("2 - Frete por Sedex - R$200.00")
+             print("0 - Retirar pedido na fábrica - R$ 0.00")
+             opcao_frete = int(input())
+
+             if opcao_frete == 1:
+                 return 100
+             elif opcao_frete == 2:
+                 return 200
+             elif opcao_frete == 0:
+                 return 0 
+             else:
+                 print("Escolha inválida, tente novamente")
+                 continue
+        except:
+                 print("Escolha inválida, tente novamente")
+                 continue
+
+
+def main():
+    modelo = escolha_modelo()
+    camiseta_num = num_camiseta()
+    valor_frete = frete()
+
+    print(f"Total: R$ {(modelo * camiseta_num) + valor_frete} (Modelo: {modelo} * Quantidade(com desconto): {modelo * camiseta_num} + frete: {valor_frete})")
+
+
+main()
